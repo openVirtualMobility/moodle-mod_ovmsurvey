c0ae4e5d324a9c91fc07b7ba8c70ed470d1f2c37
Pierre Duverneix <pierre.duverneix@gmail.com>
2019-06-12 09:38:01 +0200

    version

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2019061107;
+$plugin->version  = 2019061200;
 $plugin->requires = 2010112400;
 $plugin->component = 'mod_ovmsurvey';
 $plugin->cron = 0;
